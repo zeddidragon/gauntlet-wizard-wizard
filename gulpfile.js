6d2b02e8f77a0580676d7e26f373c5e96f04f5e6
Zeddy <zed_di_dragon@hotmail.com>
2016-03-28 13:02:35 +0200

    Removed node stuff

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp')
-var jade = require('gulp-jade')
-
-gulp.task('build', function(){
-  gulp.src('./index.jade')
-    .pipe(jade({pretty: true}))
-    .pipe(gulp.dest('./'))
-})
